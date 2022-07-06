6cd54c08d19c32a5d5a00efb150feaf79a35541f
Mert Erden <merden01@eecs.tufts.edu>
2022-07-06 12:10:39 -0400

    dev: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Installation
+
+```bash
+npm install
+```
+
+## Development
+
+First, run the development server:
+
+```bash
+npm start
+```
+
+or
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
---
+++ data/siteMetadata.js
@@ -0,0 +1,22 @@
+const siteMetadata = {
+  title: 'DIAMONDS Website :~)',
+  author: 'Foo Bar',
+  headerTitle: 'DIAMONDS',
+  description: 'A blog created with Next.js and Tailwind.css',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: '',
+  siteRepo: '',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'address@yoursite.com',
+  github: 'https://github.com',
+  twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
+  locale: 'en-US',
+}
+
+module.exports = siteMetadata
